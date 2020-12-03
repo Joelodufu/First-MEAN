3d6b82b047fd9f4f6af614be4481737c90bb87a2
joel odufu <joel.odufu@gmail.com>
2020-12-03 11:19:28 +0100

    recently added
    added one

--- app.js
+++ app.js
@@ -6,23 +6,30 @@ var mongoose = require('mongoose')
 var cors = require('cors')
 
 
-
+//IMPORTING ROUTS
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/products');
+
+
 
 var app = express();
-mongoose.connect('mongodb://localhost:27017/shopping')
+
+//CONNECTING TO MONGODB
+mongoose.connect('mongodb://localhost:27017/shopping', {useUnifiedTopology:true, useNewUrlParser:true})
     .then(response => {
     console.log('Connected');
     }).catch(err => {
         console.log('Error in connecting to thee database');
     })
 
+//ALLOWING CROSS ORIGIN ACCESS
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
     allowedHeaders:"Content-type, Authentication, Origin, X-Requested-With, Accept"
 }))
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,6 +37,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/products', usersRouter);
 
 module.exports = app;
